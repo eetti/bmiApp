7fc84b60919d0c6bfef40bc7044c960d6d4a43a0
Emmanuel Etti <etti.emman@gmail.com>
2016-03-18 23:11:34 -0300

    Initial Commit

---
+++ bmiFinal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bmiFinal
+//
+//  Created by Emmanuel Etti on 2016-03-18.
+//  Copyright © 2016 Emmanuel Etti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
